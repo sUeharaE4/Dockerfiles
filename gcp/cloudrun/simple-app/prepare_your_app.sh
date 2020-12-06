d99c89f4138a2b6a0b2fb6d589c0efe5d79ec4fd
sUeharaE4 <funa1hasi2hasi@gmail.com>
2020-12-07 00:36:22 +0900

    Modify: add sed to replace build config

--- gcp/cloudrun/simple-app/prepare_your_app.sh
+++ gcp/cloudrun/simple-app/prepare_your_app.sh
@@ -3,8 +3,14 @@
 YOUR_KMS_KEYRING=my_sample_key
 YOUR_KMS_KEY=my_sample_key
 YOUR_GCP_PROJECT_NAME=samplefordockerfiles
-TAG_NAME=gcpkms
-YOUR_APP_NAME=sample
+TAG_NAME=simpleapp
+YOUR_APP_NAME=simpleapp
 
-cp ./cloudbuild.yaml ./cloudbuild.yaml_backup
-# TODO sed
+convert_target=cloudbuild.yaml
+cp ${convert_target} ${convert_target}_backup
+
+sed -i "s/YOUR_KMS_KEYRING/${YOUR_KMS_KEYRING}/" ${convert_target}
+sed -i "s/YOUR_KMS_KEY/${YOUR_KMS_KEY}/" ${convert_target}
+sed -i "s/YOUR_GCP_PROJECT_NAME/${YOUR_GCP_PROJECT_NAME}/" ${convert_target}
+sed -i "s/TAG_NAME/${TAG_NAME}/" ${convert_target}
+sed -i "s/YOUR_APP_NAME/${YOUR_APP_NAME}/" ${convert_target}
